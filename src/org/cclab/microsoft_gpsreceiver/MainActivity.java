279d1679a51d262bf23b372b65b9a77fd0ebd5d4
ipuris <ipuris@ipuris.net>
2014-03-12 09:39:34 +0900

    Change main theme to NoTitleBar (full screen)

--- src/org/cclab/microsoft_gpsreceiver/MainActivity.java
+++ src/org/cclab/microsoft_gpsreceiver/MainActivity.java
@@ -1,13 +1,13 @@
 package org.cclab.microsoft_gpsreceiver;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
