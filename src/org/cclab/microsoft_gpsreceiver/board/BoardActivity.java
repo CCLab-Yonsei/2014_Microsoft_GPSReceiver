2885fe2789bcf7a9b40bf84cb896585c870ad6f7
gnoowik <gnoowik@yonsei.ac.kr>
2014-03-18 13:39:17 +0900

    Add pull to refrsh listview

--- src/org/cclab/microsoft_gpsreceiver/board/BoardActivity.java
+++ src/org/cclab/microsoft_gpsreceiver/board/BoardActivity.java
@@ -1,21 +1,44 @@
 package org.cclab.microsoft_gpsreceiver.board;
 
-/* Board main activity */
+/* Main activity of board */
 
 import org.cclab.microsoft_gpsreceiver.R;
 import org.cclab.microsoft_gpsreceiver.R.layout;
 import org.cclab.microsoft_gpsreceiver.R.menu;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class BoardActivity extends Activity {
+public class BoardActivity extends ListActivity {
 
+	private PullToRefreshListView listView;
+	private BoardAdapter adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_board);
+		
+		listView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+		
+		// Set listeners
+		listView.setOnRefreshListener(refreshListener);
+		listView.setOnLastItemVisibleListener(lastListener);
+		
+		adapter = new BoardAdapter()
+		
+		
 	}
 
 	@Override
@@ -25,5 +48,53 @@ public class BoardActivity extends Activity {
 				
 		return true;
 	}
+	
+	
+	/*
+	 * Listeners for listview
+	 */
+	OnRefreshListener<ListView> refreshListener = new OnRefreshListener<ListView>() {
+
+		@Override
+		public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
+	
+	OnLastItemVisibleListener lastListener = new OnLastItemVisibleListener() {
+
+		@Override
+		public void onLastItemVisible() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
+	
+	private class GetBoardsTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	
+	/*
+	* Adapter for board item
+	*/
+	private class BoardAdapter extends ArrayAdapter {
+
+		public BoardAdapter(Context context, int resource) {
+			super(context, resource);
+			// TODO Auto-generated constructor stub
+		}
+		
+	}
 
 }
+
