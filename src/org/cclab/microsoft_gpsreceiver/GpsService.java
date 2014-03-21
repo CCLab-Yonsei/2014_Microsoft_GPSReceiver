9ec06045253d57344dee10e31979b4b3a2f57dce
ipuris <ipuris@ipuris.net>
2014-03-21 17:36:23 +0900

    Remove dummy data

--- src/org/cclab/microsoft_gpsreceiver/GpsService.java
+++ src/org/cclab/microsoft_gpsreceiver/GpsService.java
@@ -91,8 +91,6 @@ public class GpsService extends Service {
 			directory.mkdirs();
 		}
 		
-		
-		dataset.add(new GpsData(1.0, 2.0, 3, 4, 5));
 		if(dataset.size() > 0) { 
 			// save data to file
 			Calendar cal = Calendar.getInstance();
