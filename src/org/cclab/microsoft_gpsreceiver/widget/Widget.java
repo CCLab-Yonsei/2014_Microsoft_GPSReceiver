addec830c6aebbc05775a0745efc95adf28a7d7f
gnoowik <gnoowik@yonsei.ac.kr>
2014-03-30 02:52:47 +0900

    Delete duplicated remoteWidgetLayoutView, Add logs in widget

--- src/org/cclab/microsoft_gpsreceiver/widget/Widget.java
+++ src/org/cclab/microsoft_gpsreceiver/widget/Widget.java
@@ -32,6 +32,7 @@ public class Widget extends AppWidgetProvider {
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 		
 		Log.i(TAG, "onUpdate()");
+		
 		for(int i=0; i < appWidgetIds.length; i++) {
 			int appWidgetId = appWidgetIds[i];
 			
@@ -46,17 +47,23 @@ public class Widget extends AppWidgetProvider {
 			// check if GPS logging service is running 
 			boolean isServiceRunning = Utility.isServiceRunning(context, GpsService.class.getName());
 			
-			RemoteViews remoteWidgetLayoutView = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 			if(isServiceRunning) {
-				remoteWidgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_off));
-				remoteWidgetLayoutView.setImageViewResource(R.id.widget_imgbtn, R.drawable.ic_action_location_off_dark);
+				Log.i(TAG, "onUpdate, isServiceRunning == true");
+				
+				widgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_off));
+				widgetLayoutView.setImageViewResource(R.id.widget_imgbtn, R.drawable.ic_action_location_off_dark);
 			}
 			else {
-				remoteWidgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_on));
-				remoteWidgetLayoutView.setImageViewResource(R.id.widget_imgbtn, R.drawable.ic_action_location_found_dark);
+				
+				Log.i(TAG, "onUpdate, isServiceRunning == false");
+				
+				widgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_on));
+				widgetLayoutView.setImageViewResource(R.id.widget_imgbtn, R.drawable.ic_action_location_found_dark);
 			}
 			
+			
 			//
+			Log.i(TAG, "onUpdate, Last Line");
 			appWidgetManager.updateAppWidget(appWidgetId, widgetLayoutView);
 		}
 	}
@@ -72,11 +79,12 @@ public class Widget extends AppWidgetProvider {
 		// check if GPS logging service is running 
 		boolean isServiceRunning = Utility.isServiceRunning(context, GpsService.class.getName());
 		if(intent.getAction().equals(Constants.INTENT_WIDGET_ACTION_BUTTON_CLICK)) {
-			
+			Log.i(TAG, "onReceive, Click Event Occur");
 			
 			// check whether GPS is enabled or not
 			final LocationManager manager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
 			if(!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+				Log.i(TAG, "onReceive, GPS Provider disabled");
 				Toast.makeText(context, R.string.main_alert_enable_gps_question, Toast.LENGTH_SHORT).show();
 				
 				Intent settingsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
@@ -85,12 +93,14 @@ public class Widget extends AppWidgetProvider {
 			}
 			else {
 				if(isServiceRunning) {
+					Log.i(TAG, "onReceive, GPS Provider enabled, isServiceRunning == true");
 					context.stopService(new Intent(context, GpsService.class));
 					remoteWidgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_off));
 					remoteWidgetLayoutView.setTextColor(R.id.widget_textview, Color.LTGRAY);
 					remoteWidgetLayoutView.setImageViewResource(R.id.widget_imgbtn, R.drawable.ic_action_location_off_dark);
 				}
 				else {
+					Log.i(TAG, "onReceive, GPS Provider enabled, isServiceRunning == false");
 					context.startService(new Intent(context, GpsService.class));
 					remoteWidgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_on));
 					remoteWidgetLayoutView.setTextColor(R.id.widget_textview, Color.WHITE);
@@ -100,18 +110,21 @@ public class Widget extends AppWidgetProvider {
 		}
 		// from start logging from MainActivity
 		else if(intent.getAction().equals(Constants.INTENT_WIDGET_CURRENTSTATE_LOGGING_ON)) {
+			Log.i(TAG, "onReceive, LOGGING ON");
 			remoteWidgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_on));
 			remoteWidgetLayoutView.setTextColor(R.id.widget_textview, Color.WHITE);
 			remoteWidgetLayoutView.setImageViewResource(R.id.widget_imgbtn, R.drawable.ic_action_location_found_dark);
 		}
 		// from finish logging from MainActivity
 		else if(intent.getAction().equals(Constants.INTENT_WIDGET_CURRENTSTATE_LOGGING_OFF)) {
+			Log.i(TAG, "onReceive, LOGGING OFF");
 			remoteWidgetLayoutView.setTextViewText(R.id.widget_textview, context.getResources().getString(R.string.widget_off));
 			remoteWidgetLayoutView.setTextColor(R.id.widget_textview, Color.LTGRAY);
 			remoteWidgetLayoutView.setImageViewResource(R.id.widget_imgbtn, R.drawable.ic_action_location_off_dark);
 		}
 		
 		// update
+		Log.i(TAG, "onReceive, Last Line");
 		(AppWidgetManager.getInstance(context)).updateAppWidget(watchWidget, remoteWidgetLayoutView);
 	 }
 	 
